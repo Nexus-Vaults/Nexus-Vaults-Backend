a51a024170db682d2bcc03551c7485039f53b3d9
Playwo <eliaswolf2001@t-online.de>
2023-06-13 19:53:33 +0200

    Add nexus existence check endpoint

---
+++ src/Application/Handlers/Queries/Nexuses/ExistenceCheck/GetNexusExistenceQuery.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation;
+using Nethereum.Util;
+using Nexus.Application.Common;
+using Nexus.Application.Services;
+using Nexus.Application.Services.Contracts;
+using System;
+using System.Threading.Tasks;
+
+namespace Nexus.Application.Handlers.Queries;
+public class GetNexusExistenceQuery
+{
+    public class Request : Query<Result>
+    {
+        public required ushort ContractChainId { get; init; }
+        public required string NexusAddress { get; init; }
+
+        public class Validator : AbstractValidator<Request>
+        {
+            public Validator()
+            {
+                RuleFor(x => x.NexusAddress)
+                    .Must(AddressUtil.Current.IsValidEthereumAddressHexFormat)
+                    .WithMessage("Invalid address format");
+            }
+        }
+    }
+
+    public enum Status : byte
+    {
+        Success,
+        UnsupportedChain,
+        NexusNotFound
+    }
+
+    public record Result(Status Status);
+
+    public class Handler : QueryHandler<Request, Result>
+    {
+        private readonly INexusFactoryProvider NexusFactoryProvider;
+        private readonly IWeb3ProviderService Web3ProviderService;
+
+        public Handler(INexusFactoryProvider nexusFactoryProvider, IWeb3ProviderService web3ProviderService)
+        {
+            NexusFactoryProvider = nexusFactoryProvider;
+            Web3ProviderService = web3ProviderService;
+        }
+
+        public override async Task<Result> HandleAsync(Request request, CancellationToken cancellationToken)
+        {
+            if (!Web3ProviderService.IsSupported(request.ContractChainId))
+            {
+                return new Result(Status.UnsupportedChain);
+            }
+
+            var nexusFactory = NexusFactoryProvider.GetInstance(request.ContractChainId);
+
+            return !await nexusFactory.HasDeployedAsync(request.NexusAddress) 
+                ? new Result(Status.NexusNotFound) 
+                : new Result(Status.Success);
+        }
+    }
+}
+
---
+++ src/WebAPI/Endpoints/Chains/Nexuses/ExistsEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Nexus.Application.Handlers.Queries;
+using Nexus.Contracts;
+using Nexus.WebAPI.Common;
+
+namespace Nexus.WebAPI.Endpoints.Chains.Nexuses;
+
+public record GetNexusExistenceRequestContract(
+    ushort ContractChainId,
+    string NexusAddress
+) : IRequestContract;
+
+public record GetNexusExistenceResponseContract(
+    bool Exists
+) : IResponseContract;
+
+[GET("Api/Chains/{contractChainId}/Nexuses/{nexusAddress}/Exists")]
+public class ExistsEndpoint : HttpEndpoint<
+    GetNexusExistenceRequestContract,
+    GetNexusExistenceQuery.Request, GetNexusExistenceQuery.Result,
+    GetNexusExistenceResponseContract>
+{
+    public override IResult MapToResponse(GetNexusExistenceQuery.Result result)
+    {
+        return result.Status switch
+        {
+            GetNexusExistenceQuery.Status.Success => Results.Ok(new GetNexusExistenceResponseContract(true)),
+            GetNexusExistenceQuery.Status.UnsupportedChain => Results.Ok(new GetNexusExistenceResponseContract(false)),
+            GetNexusExistenceQuery.Status.NexusNotFound => Results.Ok(new GetNexusExistenceResponseContract(false)),
+            _ => throw new NotImplementedException()
+        };
+    }
+}
